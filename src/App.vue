b85b750f9a7079da2c6dbcad3870d57142953631
code_tin <code_tin@msn.com>
2024-07-14 16:01:03 +0800

    openlink

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,7 @@ import {ref} from 'vue'
 let count = ref(0)
 function call(){
   WebApp.showAlert(`Hello World! Current count is ${count}`)
+  WebApp.openTelegramLink('https://t.me/share/url?text=%F0%9F%9A%80%F0%9F%9A%80%F0%9F%9A%80%E5%BC%80%E5%A7%8B%E6%B5%8B%E8%AF%95%E6%88%91%E7%9A%84%E9%93%BE%E6%8E%A5%E5%90%A7%0A&url=https://t.me/teletest3721_bot/game?startapp=3JRL8T')
 }
 </script>
 
