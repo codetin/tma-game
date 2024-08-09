40dfc883f4955ac36bdc7ca29014788899c23810
code_tin <code_tin@msn.com>
2024-08-10 00:28:51 +0800

    v2

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,14 @@
-import './assets/main.css'
+import "./assets/main.css";
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-import WebApp from '@twa-dev/sdk'
-import App from './App.vue'
-import router from './router'
-WebApp.ready()
-const app = createApp(App)
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+import WebApp from "@twa-dev/sdk";
+import App from "./App.vue";
+import router from "./router";
+WebApp.ready();
+const app = createApp(App);
 
-app.use(createPinia())
-app.use(router)
+app.use(createPinia());
+app.use(router);
 
-app.mount('#app')
+app.mount("#app");
--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,30 @@
-import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+import { fileURLToPath, URL } from "node:url";
+import { viteStaticCopy } from "vite-plugin-static-copy";
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueDevTools from "vite-plugin-vue-devtools";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools()],
+  plugins: [
+    vue(),
+    vueDevTools(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: "src/*.json", // 源 JSON 文件路径
+          dest: ".", // 目标输出目录，注意这里是 docs
+        },
+      ],
+    }),
+  ],
   build: {
-    outDir: './docs'
+    outDir: "./docs",
   },
-  base: './',
+  base: "./",
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+});
